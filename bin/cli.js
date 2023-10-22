95222f074261dc63cfe8014a1d4221e2352b6519
Robert Kieffer <robert@broofa.com>
2023-10-22 16:31:11 -0700

    feat: basic CLI file structure

---
+++ bin/cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// Thin wrapper around mime
+import cli from '../dist/npmgraph_cli.js';
+
+await cli();
---
+++ src/npmgraph_cli.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default async function () {
+  process.title = 'npmgraph';
+
+  console.log('Hello, World!');
+}
